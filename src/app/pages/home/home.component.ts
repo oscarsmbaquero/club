f7c451bd329e509d4eff17834ce97dec03b0a5e5
oscar <oscarsmbaquero@gmail.com>
2024-10-12 18:01:26 +0200

    init home anf tarifas

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,12 +1,34 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, HostListener, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import * as AOS from 'aos';
+declare var Parallax: any;
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
 
+  images: string[] = [
+    'https://res.cloudinary.com/dcfk8yjwr/image/upload/v1682962071/gi5qvq7ue2p5nvpqm7an.jpg',
+    // 'public/images/fiesta2.jpg',
+    // 'public/images/fiesta3.jpg',
+  ];
+
+  ngOnInit(): void {
+    //Called after the constructor, initializing input properties, and the first call to ngOnChanges.
+    //Add 'implements OnInit' to the class.
+    console.log(this.images);
+    
+    
+  }
+
+  ngAfterViewInit(): void {
+    setTimeout(() => {
+      AOS.refresh()
+    }, 500)
+  }
 }
