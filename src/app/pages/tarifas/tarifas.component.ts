9288d0fce90eb9612b6f4ce92ea4851015676fe2
oscar <oscarsmbaquero@gmail.com>
2024-10-12 20:21:00 +0200

    tarifas collapse ok

--- src/app/pages/tarifas/tarifas.component.ts
+++ src/app/pages/tarifas/tarifas.component.ts
@@ -5,22 +5,23 @@ import { CommonModule } from '@angular/common';
 //primeng
 import { PanelModule } from 'primeng/panel';
 import { AvatarModule } from 'primeng/avatar';
+import { ButtonModule } from 'primeng/button';
 
 @Component({
   selector: 'app-tarifas',
   standalone: true,
-  imports: [PanelModule, AvatarModule, CommonModule],
-  animations: [
-    trigger('panelContent', [
-      transition(':enter', [
-        style({ opacity: 0 }),
-        animate('0.5s', style({ opacity: 1 })),
-      ]),
-      transition(':leave', [
-        animate('0.5s', style({ opacity: 0 })),
-      ]),
-    ]),
-  ],
+  imports: [PanelModule, AvatarModule, CommonModule, ButtonModule],
+  // animations: [
+  //   trigger('panelContent', [
+  //     transition(':enter', [
+  //       style({ opacity: 0 }),
+  //       animate('0.5s', style({ opacity: 1 })),
+  //     ]),
+  //     transition(':leave', [
+  //       animate('0.5s', style({ opacity: 0 })),
+  //     ]),
+  //   ]),
+  // ],
   templateUrl: './tarifas.component.html',
   styleUrl: './tarifas.component.css'
 })
