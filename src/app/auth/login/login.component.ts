99bc529003c8a707426cbdd8c37ee4f1d2abea4e
Manuel0311Nan <manuzz3112@gmail.com>
2024-10-20 13:20:23 +0200

    design forms

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,12 +1,26 @@
 import { Component } from '@angular/core';
-
+import { FormBuilder, FormGroup, Validators, ReactiveFormsModule  } from '@angular/forms';
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
+  loginForm: FormGroup;
 
+  constructor(private fb: FormBuilder) {
+    this.loginForm = this.fb.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(6)]],
+    });
+  }
+
+  onSubmit() {
+    if (this.loginForm.valid) {
+      console.log('Login form submitted:', this.loginForm.value);
+      // Aquí puedes realizar la autenticación o llamar a un servicio
+    }
+  }
 }
