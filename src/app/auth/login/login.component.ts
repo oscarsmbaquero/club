fb9e89a061be7349a0ac8a9ac0e16786199b67b9
oscar <oscarsmbaquero@gmail.com>
2024-10-24 13:20:57 +0200

    loading login y resgister

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -4,15 +4,21 @@ import { Router } from '@angular/router';
 import { RouterModule } from '@angular/router'; 
 import { UsersService } from '../../core/services/users/users.service';
 import { NotificationService } from '../../core/services/notificationService/notificationService';
+import { LoadingComponent } from '../../shared/components/loading/loading/loading.component';
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [ReactiveFormsModule, RouterModule],
+  imports: [ReactiveFormsModule, RouterModule, LoadingComponent],
   templateUrl: './login.component.html',
   styleUrl: '../auth-styles.css'
 })
 export class LoginComponent {
   loginForm: FormGroup;
+  
+  /**
+   * flag para pintar el loading
+   */
+  loading = false;
 
   constructor(
     private fb: FormBuilder,
@@ -32,11 +38,12 @@ onSubmit() {
       mail: this.loginForm.get('email')?.value,
       password: this.loginForm.get('password')?.value,
     };
-
+    this.loading = true;
     this.usersServices.login(user).subscribe(
       (response: any) => {                
         // Manejo de la respuesta exitosa (200)
         if ( response.success) {
+          this.loading = false;
           this.notificationService.showNotification(
             'success',
             `Bienvenido, ${response.nombreUsuario}`
@@ -50,12 +57,23 @@ onSubmit() {
         // Manejo de errores
         if (error.status === 404) {
           // Usuario no encontrado
+          this.loading = false;
           console.error('User not found:', error);
           this.notificationService.showNotification(
             'error',
             `Usuario no encontrado`
           );
-        } else if (error.status === 500) {
+        } else if (error.status === 401) {
+          // Usuario no encontrado
+          this.loading = false;
+          console.error('User not found:', error);
+          this.notificationService.showNotification(
+            'error',
+            `Usuario o contraseña incorrectos`
+          );
+        }
+        else if (error.status === 500) {
+          this.loading = false;
           // Error en el servidor
           console.error('Server error:', error);
           this.notificationService.showNotification(
@@ -64,6 +82,7 @@ onSubmit() {
           );
         } else {
           // Otros errores
+          this.loading = false;
           console.error('Unexpected error:', error);
           // const translatedMessage = this.translate.instant(
           //   'NOTIFICACIONES.ERROR-DESCONOCIDO'
--- src/app/auth/register/register.component.ts
+++ src/app/auth/register/register.component.ts
@@ -7,16 +7,19 @@ import { Router } from '@angular/router';
 //servicios
 import { UsersService } from '../../core/services/users/users.service';
 import { NotificationService } from '../../core/services/notificationService/notificationService';
+//componentes
+import { LoadingComponent } from '../../shared/components/loading/loading/loading.component';
 
 @Component({
   selector: 'app-register',
   standalone: true,
-  imports: [ReactiveFormsModule, MatCardModule, MatInputModule, MatButtonModule],
+  imports: [ReactiveFormsModule, MatCardModule, MatInputModule, MatButtonModule, LoadingComponent],
   templateUrl: './register.component.html',
   styleUrl: '../auth-styles.css',
 })
 export class RegisterComponent {
   registerForm: FormGroup;
+  loading = false;
 
   constructor(
     private fb: FormBuilder,
@@ -46,6 +49,7 @@ export class RegisterComponent {
 
   onSubmit() {
     if (this.registerForm.valid) {      
+      this.loading = true;
       // Crear un objeto de usuario con los datos del formulario
       const user: any = {
         user: this.registerForm.get('user')?.value,
@@ -54,6 +58,7 @@ export class RegisterComponent {
       };
       this.usersService.register(user).subscribe(
         (response) => {
+          this.loading = false;
           this.notificationService.showNotification(
             'success',
             `Usuario registrado correctamente`
@@ -67,6 +72,7 @@ export class RegisterComponent {
         (error) => {
           // Manejo del error que envía el servidor
           if (error.status === 500) {
+            this.loading = false;
             this.notificationService.showNotification(
               'error',
               'Este mail ya esta regitrado en nuestro sistema'
