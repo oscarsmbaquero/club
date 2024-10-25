a888fb5259a474cf0329de64777428b8b289e8d6
oscar <oscarsmbaquero@gmail.com>
2024-10-25 12:33:21 +0200

    avatar profile ok

--- src/app/pages/profile/profile/profile.component.ts
+++ src/app/pages/profile/profile/profile.component.ts
@@ -4,18 +4,22 @@ import { UsersService } from '../../../core/services/users/users.service';
 import { LoadingComponent } from '../../../shared/components/loading/loading.component';
 //pipe
 import { DefaultValuePipe } from '../../../shared/pipes/default-value.pipe';
+//primeng
+import { AvatarModule } from 'primeng/avatar';
 
 @Component({
   selector: 'app-profile',
   standalone: true,
-  imports: [LoadingComponent, DefaultValuePipe],
+  imports: [LoadingComponent, DefaultValuePipe, AvatarModule],
   templateUrl: './profile.component.html',
   styleUrl: './profile.component.css'
 })
 export class ProfileComponent {
   //TODO TIPAR CUANDO TENGAMOS TODOS LOS DATOS DEL CLIENTE
   user: any;
+  userName = '';
   loading = false;
+  palabrasAvatar = '';
 
   constructor(
     private usersService: UsersService
@@ -32,6 +36,8 @@ export class ProfileComponent {
   getUserId(): string | null {
     // Recupera y parsea el ID del usuario desde localStorage
     const userData = localStorage.getItem("user");
+    const userName = userData ? JSON.parse(userData).data.user : '';
+    this.lettersAvatar(userName);
     return userData ? JSON.parse(userData).data.id : null;
   }
 
@@ -40,6 +46,24 @@ export class ProfileComponent {
       this.loading = false;
       this.user= element;
       
+      
     })
+  }
+
+   /**
+   * funcion para pintar el nombre del avatar, solo la primera letra de las dos primeras palabras
+   * @param cadena
+   * @param cantidadLetras
+   */
+   lettersAvatar(cadena: string, cantidadLetras = 1) {
+    const palabras = cadena.split(' ');
+    // Solo las dos primeras palabras
+    const primerasDosPalabras = palabras.slice(0, 2);
+    // Iterar sobre cada palabra y extraer las letras especificadas
+    const letrasExtraidas = primerasDosPalabras.map((palabra) => {
+      return palabra.slice(0, cantidadLetras);
+    });
+    const resultado = letrasExtraidas.join('');
+    this.palabrasAvatar = resultado;
   }
 }
