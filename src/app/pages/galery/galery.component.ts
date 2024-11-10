7beeee6b5aba61afde9970d347e9085c83d4f370
Manuel0311Nan <manuzz3112@gmail.com>
2024-11-10 19:09:51 +0100

    add option for gallery

--- src/app/pages/galery/galery.component.ts
+++ src/app/pages/galery/galery.component.ts
@@ -8,5 +8,14 @@ import { Component } from '@angular/core';
   styleUrl: './galery.component.css'
 })
 export class GaleryComponent {
-
+  sliderImagesDesktop = [
+    "https://res.cloudinary.com/de3ujeyub/image/upload/v1731260834/erika-m-Zb9azmtuYsk-unsplash_ppzqx6.jpg",
+    "https://res.cloudinary.com/de3ujeyub/image/upload/v1731260834/stock-birken-mqBf5REZCS8-unsplash_lxwnes.jpg",
+    "https://res.cloudinary.com/de3ujeyub/image/upload/v1731260834/luca-pennacchioni-Imq_p7S3pLg-unsplash_lytnrs.jpg",
+    "https://res.cloudinary.com/de3ujeyub/image/upload/v1731260834/artists-eyes-Ej_FGjd-DaY-unsplash_qzffl1.jpg",
+    "https://res.cloudinary.com/de3ujeyub/image/upload/v1731260834/erika-m-Zb9azmtuYsk-unsplash_ppzqx6.jpg",
+    "https://res.cloudinary.com/de3ujeyub/image/upload/v1731260834/stock-birken-mqBf5REZCS8-unsplash_lxwnes.jpg",
+    "https://res.cloudinary.com/de3ujeyub/image/upload/v1731260834/luca-pennacchioni-Imq_p7S3pLg-unsplash_lytnrs.jpg",
+    "https://res.cloudinary.com/de3ujeyub/image/upload/v1731260834/artists-eyes-Ej_FGjd-DaY-unsplash_qzffl1.jpg",
+  ]
 }
